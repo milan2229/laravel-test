6e02e8661af6fe067125cb2f5b0e6862be73516c
y.neishi <y.neishi@preapp.jp>
2021-09-17 10:08:26 +0900

    [add] bootstrap共通化の為layoutディレクトリ作成

--- src/laraveltest/resources/views/user/edit.blade.php
+++ src/laraveltest/resources/views/user/edit.blade.php
@@ -1,11 +1,8 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-        crossorigin="anonymous"></script>
+@extends('layout.layout')
+
 <div class="container">
     <h1>Edit</h1>
-    <a class="btn btn-primary btn-lg" href="{{ route('user.index') }}" role="button">User All</a>
+    <a class="btn btn-warning btn-lg" href="{{ route('user.index') }}" role="button">User All</a>
     <form method="POST" action="{{ route('user.update',$user->id) }}">
         <input type="hidden" name="_method" value="PUT">
         @csrf
@@ -21,7 +18,7 @@
         </div>
 
         <div>
-            <label for="form-email">メールアドレス</label>
+            <label for="form-email">email</label>
             <input type="email" name="email" value="{{ $user->email }}" class="col-form-label form-control">
         </div>
         <div>
--- src/laraveltest/resources/views/user/index.blade.php
+++ src/laraveltest/resources/views/user/index.blade.php
@@ -1,11 +1,5 @@
-<head>
-    <title>Laravel test</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-</head>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-        crossorigin="anonymous"></script>
+@extends('layout.layout')
+
 <div class="container">
     <a class="btn btn-primary btn-lg" href="{{ route('user.create') }}" role="button">NEW</a>
     <table class="table">
@@ -32,7 +26,7 @@
                     <form method="POST" action="{{route('user.destroy',$user->id)}}">
                         @csrf
                         @method('delete')
-                        <button type="submit" class="btn btn-danger">削除</button>
+                        <button type="submit" class="btn btn-danger">delete</button>
                 </td>
                 </form>
             </tr>
