547e0f605e8f481e92dc57b689e18b705fbeeae7
y.neishi <y.neishi@preapp.jp>
2021-09-19 22:54:51 +0900

    [update] factory使用しテストデータ20件投入

--- src/laraveltest/database/seeders/UsersTableSeeder.php
+++ src/laraveltest/database/seeders/UsersTableSeeder.php
@@ -13,23 +13,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => 'user01',
-                'email' => 'user01@gmail.com',
-                'birthday' => '1980/11/22',
-            ],
-            [
-                'name' => 'user02',
-                'email' => 'user02@gmail.com',
-                'birthday' => '1980/11/22',
-            ],
-            [
-                'name' => 'user03',
-                'email' => 'user03@gmail.com',
-                'birthday' => '1980/11/22',
-            ]
 
-        ]);
     }
 }
